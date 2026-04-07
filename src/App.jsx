74e7ee00a2787f73b8d3bfcd2c67edede6ce5532
Demi Sokoya <68524557+demi-sokoya@users.noreply.github.com>
2026-04-06 23:11:05 -0600

    feat: add spacing elements in App component and remove redundant padding in CSS

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./components/NavBar";
 import CurrentWeather from "./components/CurrentWeather";
 import HourlyForecast from "./components/HourlyForecast";
 import DailyForecast from "./components/DailyForecast";
+import { Spacer, VStack, Box } from "@chakra-ui/react";
 
 function App() {
 	const {
@@ -23,10 +24,13 @@ function App() {
 	return (
 		<>
 			<Navbar />
+			<Box h="2rem" />
 			{loading && <p>Loading...</p>}
 			{error && <p>Error; {error}</p>}
 			<CurrentWeather />
+			<Box h="2rem" />
 			<HourlyForecast />
+			<Box h="2rem" />
 			<DailyForecast />
 		</>
 	);
